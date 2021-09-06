4fa30c125051320f20ade7c9c48728d6177e7dcc
Manuel <manuel@thincrs.com>
2021-09-06 18:12:45 -0500

    Remove elements

--- src/previews/button/index.tsx
+++ src/previews/button/index.tsx
@@ -5,14 +5,7 @@ const Home: React.FC<any> = () => {
   return (
     <div>
       <div className="example">
-        <Example title="Button sizes">
-        </Example>
-        <Example title="Button types">
-        </Example>
-        <Example title="Loading button">
-        </Example>
-        <Example title="Disabled button">
-        </Example>
+        
       </div>
     </div>
   )

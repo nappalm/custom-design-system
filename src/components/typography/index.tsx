6d5b4453d569208d72dba38d01113aa448cc82ec
Manuel <manuel@thincrs.com>
2021-09-13 07:21:21 -0500

    Paragraph component

--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -3,11 +3,13 @@ import './index.less';
 
 import Text from './text';
 import Title from './title';
+import Paragraph from './paragraph';
 import Link from './link';
 
 export type TypographyProps = typeof Origin & {
   Text: typeof Text;
   Title: typeof Title;
+  Paragraph: typeof Paragraph;
   Link: typeof Link;
 }
 
@@ -15,6 +17,7 @@ const Typography = Origin as TypographyProps;
 
 Typography.Text = Text;
 Typography.Title = Title;
+Typography.Paragraph = Paragraph;
 Typography.Link = Link;
 
 export default Typography;
--- src/previews/typography/index.tsx
+++ src/previews/typography/index.tsx
@@ -61,6 +61,45 @@ const Preview: React.FC<any> = () => (
           h5 Typography
         </Typography.Title>
       </Example>
+      <Example title="Typography Paragraph">
+        <Typography.Paragraph>
+          Es un hecho establecido hace demasiado tiempo que un
+          lector se distraerá con el contenido del texto de un
+          sitio mientras que mira su diseño. El punto de usar
+          Lorem Ipsum es que tiene una distribución más o menos
+          normal de las letras, al contrario de usar textos como
+          por ejemplo Contenido aquí, contenido aquí.
+        </Typography.Paragraph>
+      </Example>
+      <Example title="Typography Paragraph Ellipsis">
+        <Typography.Paragraph ellipsis>
+          Es un hecho establecido hace demasiado tiempo que un
+          lector se distraerá con el contenido del texto de un
+          sitio mientras que mira su diseño. El punto de usar
+          Lorem Ipsum es que tiene una distribución más o menos
+          normal de las letras, al contrario de usar textos como
+          por ejemplo Contenido aquí, contenido aquí.
+        </Typography.Paragraph>
+      </Example>
+      <Example title="Typography Paragraph Ellipsis Multiline">
+        <Typography.Paragraph ellipsis={{ rows: 3, expand: false }}>
+          Es un hecho establecido hace demasiado tiempo que un
+          lector se distraerá con el contenido del texto de un
+          sitio mientras que mira su diseño. El punto de usar
+          Lorem Ipsum es que tiene una distribución más o menos
+          normal de las letras, al contrario de usar textos como
+          por ejemplo Contenido aquí, contenido aquí.
+          Estos textos hacen parecerlo un español que se puede leer.
+          Muchos paquetes de autoedición y editores de páginas web
+          usan el Lorem Ipsum como su texto por defecto,
+          y al hacer una búsqueda de Lorem Ipsum va a dar
+          por resultado muchos sitios web que usan este
+          texto si se encuentran en estado de desarrollo.
+          Muchas versiones han evolucionado a través de los años,
+          algunas veces por accidente, otras veces a
+          (por ejemplo insertándole humor y cosas por el estilo).
+        </Typography.Paragraph>
+      </Example>
     </div>
   </div>
 );

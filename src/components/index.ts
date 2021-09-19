c1a7046812b38d04aa660785e6fbf8d7e00690fd
Manuel <manuel@thincrs.com>
2021-09-19 12:00:41 -0500

    Exported component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,6 @@ export { default as Button } from './button';
 export { default as Typography } from './typography';
 export { default as Row } from './row';
 export { default as Col } from './col';
+export { default as Dropdown } from './dropdown';
+export { default as Input } from './input';
+export { default as Radio } from './radio';
